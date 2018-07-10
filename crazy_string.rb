6fdd82dec53de6392da318a8b5de5e7472404db9
AnnabelleCat <pikainka@gmail.com>
2018-07-10 21:24:35 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,7 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings (a,b)
+  "#{a}".reverse.upcase + " #{b}".gsub(/s/,'Z') .upcase.downcase
 end
+
+puts crazy_strings("cat", "meows")
