d696b624a6df2eab6072de511a2fa45943c9f488
AnnabelleCat <pikainka@gmail.com>
2018-07-10 21:26:49 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 # write your method here
 
 def crazy_strings (a,b)
-  "#{a}".reverse.upcase + " #{b}".gsub(/s/,'Z') .swapcase
+  "#{a}".reverse.upcase + " #{b}".gsub(/S/,'Z') .swapcase
 end
 
 puts crazy_strings("AA", "bb")
